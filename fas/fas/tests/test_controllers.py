bd00d239c0e35b8d1fad83359ca1deff54f71b1d
Toshio Kuratomi <badger@Clingman.(none)>
2008-02-16 12:13:10 -0800

    Updates from TG-1.0.4.2

--- fas/fas/tests/test_controllers.py
+++ fas/fas/tests/test_controllers.py
@@ -1,33 +1,37 @@
+import unittest
 import turbogears
-from nose import with_setup
 from turbogears import testutil
 from fas.controllers import Root
 import cherrypy
 
-def teardown_func():
-    """Tests for apps using identity need to stop CP/TG after each test to
-    stop the VisitManager thread. See http://trac.turbogears.org/turbogears/ticket/1217
-    for details.
-    """
-    turbogears.startup.stopTurboGears()
-
 cherrypy.root = Root()
 
-def test_method():
-    "the index method should return a string called now"
-    import types
-    result = testutil.call(cherrypy.root.index)
-    assert type(result["now"]) == types.StringType
-test_method = with_setup(teardown=teardown_func)(test_method)
+class TestPages(unittest.TestCase):
 
-def test_indextitle():
-    "The indexpage should have the right title"
-    testutil.createRequest("/")
-    assert "<TITLE>Welcome to TurboGears</TITLE>" in cherrypy.response.body[0]
-test_indextitle = with_setup(teardown=teardown_func)(test_indextitle)
+    def setUp(self):
+        turbogears.startup.startTurboGears()
 
-def test_logintitle():
-    "login page should have the right title"
-    testutil.createRequest("/login")
-    assert "<TITLE>Login</TITLE>" in cherrypy.response.body[0]
-test_logintitle = with_setup(teardown=teardown_func)(test_logintitle)
+    def tearDown(self):
+        """Tests for apps using identity need to stop CP/TG after each test to
+        stop the VisitManager thread. 
+        See http://trac.turbogears.org/turbogears/ticket/1217 for details.
+        """
+        turbogears.startup.stopTurboGears()
+
+    def test_method(self):
+        "the index method should return a string called now"
+        import types
+        result = testutil.call(cherrypy.root.index)
+        assert type(result["now"]) == types.StringType
+
+    def test_indextitle(self):
+        "The indexpage should have the right title"
+        testutil.createRequest("/")
+        response = cherrypy.response.body[0].lower() 
+        assert "<title>welcome to turbogears</title>" in response
+
+    def test_logintitle(self):
+        "login page should have the right title"
+        testutil.createRequest("/login")
+        response = cherrypy.response.body[0].lower()
+        assert "<title>login</title>" in response
