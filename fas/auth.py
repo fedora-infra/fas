4fb7439b129c22a72dffd48677e60ecafb7a5187
Ricky Zhou <ricky@fedoraproject.org>
2008-02-27 15:16:30 -0500

    Clean up user list.

--- fas/auth.py
+++ fas/auth.py
@@ -126,8 +126,6 @@ def canCreateGroup(person, group):
     '''
     Returns True if the user can create groups
     '''
-    turbogears.flash('Bypass in auth.py.  Fix!')
-    return True
     # Should groupname restrictions go here?
     if isAdmin(person):
         return True
