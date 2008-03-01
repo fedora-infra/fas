688f12dca7a59d68215cc029dab03984e278e3a6
Michael McGrath <mmcgrath@desktop.mmcgrath.net>
2008-03-01 11:33:14 -0600

    Added broken links

--- fas/fas/help.py
+++ fas/fas/help.py
@@ -16,7 +16,7 @@ class Help(controllers.Controller):
             'user_primary_email' : ['Primary Email (Required)', 'This email address should be your prefered email contact and will be used to send various official emails to.  This is also where your @fedoraproject.org email will get forwarded'],
             'user_human_name' :    ['Full Name (Required)', 'Your Human Name or "real life" name'],
             'user_gpg_keyid' :     ['GPG Key', 'Only required for users signing the CLA.  It is generally used to prove that a message or email came from you or to encrypt information so that only the recipients can read it.  See http://fedoraproject.org/wiki/Infrastructure/AccountSystem/CLAHowTo for more information'],
-            'user_telephone' :     ['Telephone', 'Only required for users signing the CLA.  Sometimes during a time of emergency someone from the Fedora Project may need to contact you.  For more information see our Privacy Policy'], 
+            'user_telephone' :     ['Telephone', 'Only required for users signing the <a href="">CLA</a>.  Sometimes during a time of emergency someone from the Fedora Project may need to contact you.  For more information see our <a href="">Privacy Policy</a>'], 
             }
         except KeyError:
             return dict(title='Error', help='We could not find that help item')
