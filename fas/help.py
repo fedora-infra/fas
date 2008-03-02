6bb11763c67f60f0671a821451e59357c60c4d68
Michael McGrath <mmcgrath@desktop.mmcgrath.net>
2008-03-01 22:46:36 -0600

    Added some more online help

--- fas/help.py
+++ fas/help.py
@@ -11,6 +11,11 @@ class Help(controllers.Controller):
             'user_human_name' :    ['Full Name (Required)', '<p>Your Human Name or "real life" name</p>'],
             'user_gpg_keyid' :     ['GPG Key', '<p>Only required for users signing the <a href="http://fedoraproject.org/wiki/Legal/Licenses/CLA">CLA</a>.  It is generally used to prove that a message or email came from you or to encrypt information so that only the recipients can read it.  See the <a href="http://fedoraproject.org/wiki/Infrastructure/AccountSystem/CLAHowTo">CLAHowTo</a> for more information</p>'],
             'user_telephone' :     ['Telephone', '<p>Only required for users signing the <a href="http://fedoraproject.org/wiki/Legal/Licenses/CLA">CLA</a>.  Sometimes during a time of emergency someone from the Fedora Project may need to contact you.  For more information see our <a href="http://fedoraproject.org/wiki/Legal/PrivacyPolicy">Privacy Policy</a></p>'],
+            'user_postal_address': ['Postal Address', '<p>Only required for users signing the <a href="http://fedoraproject.org/wiki/Legal/Licenses/CLA">CLA</a>.  This should be a mailing address where you can be contacted.  See our <a href="http://fedoraproject.org/wiki/Legal/PrivacyPolicy">Privacy Policy</a> about any concerns.</p>'],
+            'user_timezone':       ['Timezone (Optional)', '<p>Please specify the time zone you are in.</p>'],
+            'user_comments':       ['Comments (Optional)', '<p>Misc comments about yourself.</p>'],
+            'user_account_status': ['Account Status', '<p>Shows account status, possible values include<ul><li>Valid</li><li>Disabled</li><li>Expired</li></ul></p>'],
+            'user_cla' :           ['CLA', '<p>In order to become a full Fedora contributor you must sign a <a href="http://fedoraproject.org/wiki/Legal/Licenses/CLA">Contributor License Agreement</a>.  This license is a legal agreement between you and Red Hat.  Full status allows people to contribute content and code and is recommended for anyone interested in getting involved in the Fedora Project.  To find out more, see the <a href="http://fedoraproject.org/wiki/Infrastructure/AccountSystem/CLAHowTo">CLAHowTo</a>.</p>'],
             }
 
     def __init__(self):
