22eb4c853c24d29776c45113e6614f4cbf2ba881
Michael McGrath <mmcgrath@desktop.mmcgrath.net>
2008-03-07 17:27:09 -0600

    This worked, but had a pretty high failure rate.  Took about 30 minutes too on my workstation

--- convert.py
+++ convert.py
@@ -13,6 +13,9 @@ cherrypy.lowercase_api = True
 from os.path import *
 import sys
 import time
+import crypt
+import random
+
 if len(sys.argv) > 1:
     update_config(configfile=sys.argv[1],
         modulename="fas.config")
@@ -25,9 +28,20 @@ from sqlalchemy import *
 from sqlalchemy.exceptions import *
 from fas.model import *
 
+
+def generate_salt(length=8):
+    chars = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    salt = ''
+    for i in xrange(length):
+        salt += random.choice(chars)
+    return salt
+
+
+
 db = pgdb.connect(dsn='localhost', user='fedora', password='test', database='fedorausers')
 
 c = db.cursor()
+
 c.execute('select id, username, email, human_name, gpg_keyid, ssh_key, password, comments, postal_address, telephone, affiliation, creation, approval_status, internal_comments, ircnick from person order by id;')
 
 print "Converting People Table"
@@ -40,7 +54,7 @@ for person in c.fetchall():
     p.human_name = human_name
     p.gpg_keyid = gpg_keyid
     p.ssh_key = ssh_key
-    p.password = password
+    p.password = crypt.crypt(password, "$1$%s" % generate_salt(8))
     p.comments = comments
     p.postal_address = postal_address
     p.telephone = telephone
@@ -74,9 +88,11 @@ for person in c.fetchall():
     email_purpose.purpose = 'primary'
     session.flush()
 
-c.execute('select id, name, owner_id, group_type, needs_sponsor, user_can_remove, prerequisite_id, joinmsg from project_group order by id;')
-bool_dict = {'f' : False, 't' : True}
+c.execute('select id, name, owner_id, group_type, needs_sponsor, user_can_remove, prerequisite_id, joinmsg from project_group;')
+bool_dict = {0 : False, 1 : True}
 print "Creating Groups..."
+admin = People.by_username('admin')
+admin_id = admin.id
 for group in c.fetchall():
     (id, name, owner_id, group_type, needs_sponsor, user_can_remove, prerequisite_id, joinmsg) = group
     print "%i - %s" % (id, name)
@@ -85,27 +101,42 @@ for group in c.fetchall():
         group.id = id
         group.name = name
         group.display_name = name
+        if owner_id == 100001:
+            ''' Update to new admin id '''
+            owner_id = admin_id
         group.owner_id = owner_id
         group.group_type = group_type
-        group.needs_sponsor = bool(bool_dict(needs_sponsor))
-        group.user_can_remove = bool(bool_dict(user_can_remove))
+        group.needs_sponsor = bool(bool_dict[needs_sponsor])
+        group.user_can_remove = bool(bool_dict[user_can_remove])
         if prerequisite_id:
             prerequisite = Groups.by_id(prerequisite_id)
             group.prerequisite = prerequisite
         group.joinmsg = joinmsg
         # Log here
         session.flush()
-    except TypeError:
-        print "The group: '%s' could not be created." % groupname
+    except IntegrityError, e:
+        print "The group: '%s' (%i) could not be created - %s" % (name, id, e)
+    except FlushError, e:
+        print "The group: '%s' (%i) could not be created - %s" % (name, id, e)
+    except InvalidRequestError, e:
+        print "The group: '%s' (%i) could not be created - %s" % (name, id, e)
+
+    session.close()
 
 c.execute('select person_id, project_group_id, role_type, role_domain, role_status, internal_comments, sponsor_id, creation, approval from role order by person_id;')
 print "Creating Role Maps..."
 for role in c.fetchall():
     (person_id, project_group_id, role_type, role_domain, role_status, internal_comments, sponsor_id, creation, approval) = role
     print "%s - %s" % (person_id, project_group_id)
-    role = PersonRoles()
-    role.role_status = role_status
-    role.role_type = role_type
-    role.member = People.by_id(person_id)
-    role.group = Group.by_id(group_id)
-    
+    try:
+        role = PersonRoles()
+        role.role_status = role_status
+        role.role_type = role_type
+        role.member = People.by_id(person_id)
+        role.group = Groups.by_id(project_group_id)
+        session.flush()
+    except ProgrammingError, e:
+        print "The role %s -> %s could not be created - %s" % (person_id, project_group_id, e)
+    except IntegrityError, e:
+        print "The role %s -> %s could not be created - %s" % (person_id, project_group_id, e)
+    session.close()
