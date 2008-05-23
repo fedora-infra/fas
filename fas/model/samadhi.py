f6621104ad5c93c25a8aed3339e01830677ca37b
Ricky Zhou <ricky@fedoraproject.org>
2008-05-23 18:54:44 -0400

    Merge changes from Samadhi (more compliance with More compliance with http://openidenabled.com/files/python-openid/docs/2.1.1/openid.store.interface.OpenIDStore-class.html#getAssociation)

--- fas/model/samadhi.py
+++ fas/model/samadhi.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Samadhi.  If not, see <http://www.gnu.org/licenses/>.
 
+import time
 from datetime import datetime
 from turbogears.database import metadata, mapper, session
 # import some basic SQLAlchemy classes for declaring the data model
@@ -24,33 +25,19 @@ from sqlalchemy import Table, Column, ForeignKey
 from sqlalchemy.orm import relation
 # import some datatypes for table columns from SQLAlchemy
 # (see http://www.sqlalchemy.org/docs/04/types.html for more)
-from sqlalchemy import String, Unicode, Integer, DateTime, PrimaryKeyConstraint
+from sqlalchemy import String, Unicode, Integer, DateTime, Binary, PrimaryKeyConstraint
 from turbogears import identity
 
 from sqlalchemy.types import TypeEngine
 
 from openid.association import Association as OpenIDAssociation
 from openid.store.interface import OpenIDStore
-
-# create our own custom type to keep from running afoul of
-# SQLAlchemy's automatic UTF-8 encoding/decoding.
-class HexString(TypeEngine):
-    def __init__(self, length):
-        self.length = length
-
-    def get_col_spec(self):
-        return 'VARCHAR(%i)' % (self.length * 2)
-
-    def convert_bind_param(self, value, engine):
-        return value.encode('hex')
-
-    def convert_result_value(self, value, engine):
-        return value.decode('hex')
+from openid.store import nonce
 
 samadhi_associations_table = Table('samadhi_associations', metadata,
                                    Column('server_url',String(2048), nullable = False),
                                    Column('handle', String(128), nullable = False),
-                                   Column('secret', HexString(128), nullable = False),
+                                   Column('secret', Binary(128), nullable = False),
                                    Column('issued', Integer, nullable = False),
                                    Column('lifetime', Integer, nullable = False),
                                    Column('assoc_type', String(64), nullable = False),
@@ -60,9 +47,8 @@ class SamadhiAssociation(object):
     @classmethod
     def get(cls, server_url, handle = None):
         if handle is None:
-            return cls.query.filter_by(server_url = server_url).order_by(issued.desc()).first()
-        else:
-            return cls.query.filter_by(server_url = server_url, handle = handle).order_by(issued.desc()).first()
+            return cls.query.filter_by(server_url = server_url).order_by(samadhi_associations_table.c.issued.desc())
+        return cls.query.filter_by(server_url = server_url, handle = handle).order_by(samadhi_associations_table.c.issued.desc())
 
     @classmethod
     def remove(cls, server_url, handle):
@@ -72,9 +58,9 @@ class SamadhiAssociation(object):
 mapper(SamadhiAssociation, samadhi_associations_table)
 
 samadhi_nonces_table = Table('samadhi_nonces', metadata,
-                             Column('server_url', HexString(2048), nullable = False),
+                             Column('server_url', String(2048), nullable = False),
                              Column('timestamp', Integer, nullable = False),
-                             Column('salt', HexString(40), nullable = False),
+                             Column('salt', String(40), nullable = False),
                              PrimaryKeyConstraint('server_url', 'timestamp', 'salt'))
 
 class SamadhiNonce(object):
@@ -97,26 +83,43 @@ class SamadhiStore(OpenIDStore):
                                assoc_type = association.assoc_type)
 
     def getAssociation(self, server_url, handle=None):
-        a = SamadhiAssociation.get(server_url, handle)
-        if a:
-            return OpenIDAssociation(a.handle,
-                                     a.secret,
-                                     a.issued,
-                                     a.lifetime,
-                                     a.assoc_type)
+        results = SamadhiAssociation.get(server_url, handle)
+        for a in results:
+            assoc = OpenIDAssociation(a.handle,
+                                      a.secret,
+                                      a.issued,
+                                      a.lifetime,
+                                      a.assoc_type)
+            if assoc.getExpiresIn() == 0:
+                SamadhiAssociation.remove(server_url, assoc.handle)
+            else:
+                return assoc
         return None
 
     def removeAssociation(self, server_url, handle):
         SamadhiAssociation.remove(server_url, handle)
 
     def useNonce(self, server_url, timestamp, salt):
-        n = SamadhiNonce(server_url = server_url,
-                         timestamp = timestamp,
-                         salt = salt)
+        if abs(timestamp - time.time()) > nonce.SKEW:
+            return False
+        results = SamadhiNonce.query.filter_by(server_url=server_url, timestamp=timestamp, salt=salt).all()
+        if results:
+            return False
+        else:
+            n = SamadhiNonce(server_url = server_url,
+                timestamp = timestamp,
+                salt = salt)
+            return True
 
     def cleanupNonces(self):
-        return 0
+        result = samadhi_nonces_table.delete(
+            samadhi_nonces_table.c.timestamp.__lt__(time.time())
+            ).execute()
+        return result.rowcount
 
     def cleanupAssociations(self):
-        return 0
+        result = samadhi_associations_table.delete(
+            samadhi_associations_table.c.issued.__add__(samadhi_associations_table.c.lifetime).__lt__(time.time())
+            ).execute()
+        return result.rowcount
 
