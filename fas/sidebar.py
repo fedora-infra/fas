befe8e926f32b9e7b158e265956d70f32d657659
Ignacio Vazquez-Abrams <ivazqueznet+git@gmail.com>
2008-06-06 12:17:41 -0400

    Allow the sidebar to be modified on the fly

---
+++ fas/sidebar.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import turbogears
+from turbogears import controllers, expose, paginate, identity, redirect, widgets, validate, validators, error_handler
+
+# A list of functions to call to get the various entries that
+#   should be shown in the sidebar
+entryfuncs = []
+
+# Generator that returns each of the entries
+def getEntries():
+    for f in entryfuncs:
+        entries = f()
+        for entry in entries:
+            yield entry
+
+
+# Stock entries used by FAS everywhere
+def stockentries():
+    entries = []
+    if not identity.current.anonymous and (
+            'sysadmin' in identity.current.groups
+            or 'accounts' in identity.current.groups
+        ):
+        entries.append(('New Group', '/group/new'))
+        entries.append(('User List', '/user/list'))
+    if not identity.current.anonymous:
+        entries.append(('Group List', '/group/list/A*'))
+        entries.append(('Apply for a new Group', '/group/list/A*'))
+    return entries
+
+entryfuncs.append(stockentries)
