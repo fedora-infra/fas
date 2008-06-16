095ea6c8e7466fae703cc8429a90bcac8f40f25c
Ricky Zhou <ricky@fedoraproject.org>
2008-06-16 01:19:35 -0400

    Make a "release", with plugins (this was mainly a mock release for the sake of testing on pt10, so feel free to rebump)

--- plugins/asterisk_plugin/setup.py
+++ plugins/asterisk_plugin/setup.py
@@ -6,21 +6,18 @@ from turbogears.finddata import find_package_data, standard_exclude, \
         standard_exclude_directories
 import os, glob
 
-excludeFiles = ['*.cfg.in']
+excludeFiles = []
 excludeFiles.extend(standard_exclude)
-excludeDataDirs = ['']
+excludeDataDirs = []
 excludeDataDirs.extend(standard_exclude_directories)
 
-package_data = find_package_data(where='./', package='fas_asterisk', exclude=excludeFiles, exclude_directories=excludeDataDirs,)
-data_files = [('__init__.py', filter(os.path.isfile, glob.glob('__init__.py'))),
-              ('templates', filter(os.path.isfile, glob.glob('templates/*html')))
-]
+package_data = find_package_data(where='fas_asterisk', package='fas_asterisk', exclude=excludeFiles, exclude_directories=excludeDataDirs,)
+
 setup(
-    name = "FAS Asterisk Plugin",
+    name = "fas-asterisk_plugin",
     version = "0.1",
     packages = find_packages(),
 
-    data_files = data_files,
     package_data = package_data,
         
     author = "Mike McGrath",
