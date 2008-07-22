4a6b404aadf0ddeb36c69a8f116c507c3160ec70
Toshio Kuratomi <toshio@fedoraproject.org>
2008-07-21 20:04:18 -0700

    Make the new column non-null and default to False.

--- database/versions/2/2.py
+++ database/versions/2/2.py
@@ -1,12 +1,12 @@
 # 
-from sqlalchemy import Table, Column, MetaData, Boolean
+from sqlalchemy import Table, Column, MetaData, Boolean, PassiveDefault, text
 from migrate import migrate_engine
 from migrate.changeset import create_column, drop_column
 
 metadata = MetaData(migrate_engine)
 
 PeopleTable = Table('people', metadata, autoload=True)
-col = Column("privacy", Boolean)
+col = Column('privacy', Boolean, PassiveDefault(text('False')), nullable=False)
 
 def upgrade():
     create_column(col, PeopleTable)
