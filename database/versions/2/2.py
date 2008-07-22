bdfa16d2b0159d4eb478185f31dd2019ebad5e0c
Yaakov M. Nemoy <loupgaroublond@gmail.com>
2008-07-22 16:12:57 +0200

    Revert "Make the new column non-null and default to False."
    Again, this is the wrong way to change this.
    
    This reverts commit 4a6b404aadf0ddeb36c69a8f116c507c3160ec70.

--- database/versions/2/2.py
+++ database/versions/2/2.py
@@ -1,12 +1,12 @@
 # 
-from sqlalchemy import Table, Column, MetaData, Boolean, PassiveDefault, text
+from sqlalchemy import Table, Column, MetaData, Boolean
 from migrate import migrate_engine
 from migrate.changeset import create_column, drop_column
 
 metadata = MetaData(migrate_engine)
 
 PeopleTable = Table('people', metadata, autoload=True)
-col = Column('privacy', Boolean, PassiveDefault(text('False')), nullable=False)
+col = Column("privacy", Boolean)
 
 def upgrade():
     create_column(col, PeopleTable)
