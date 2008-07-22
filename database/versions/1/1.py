29f9074893bb848f26c9c48a20fceb0d65be7897
Yaakov M. Nemoy <loupgaroublond@gmail.com>
2008-07-22 15:32:01 +0200

    Revert "Text => UnicodeText"
    Never never never ever edit a file once in it's in the DB repo on basis of 'substance'.  If a change to the DB is correct, please issue a new update to the repo.  This will be more obvious in the next patch.
    
    This reverts commit d287e08e928beb7ecf01aa9210f35a660a32f675.

--- database/versions/1/1.py
+++ database/versions/1/1.py
@@ -1,12 +1,12 @@
 # 
-from sqlalchemy import Table, Column, MetaData, UnicodeText
+from sqlalchemy import Table, Column, MetaData, Text
 from migrate import migrate_engine
 from migrate.changeset import create_column, drop_column
 
 metadata = MetaData(migrate_engine)
 
 GroupsTable = Table('groups', metadata, autoload=True)
-col = Column('apply_rules', UnicodeText)
+col = Column("apply_rules", Text)
 
 def upgrade():
 	create_column(col, GroupsTable)
