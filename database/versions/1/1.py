d287e08e928beb7ecf01aa9210f35a660a32f675
Toshio Kuratomi <toshio@fedoraproject.org>
2008-07-21 20:03:41 -0700

    Text => UnicodeText

--- database/versions/1/1.py
+++ database/versions/1/1.py
@@ -1,12 +1,12 @@
 # 
-from sqlalchemy import Table, Column, MetaData, Text
+from sqlalchemy import Table, Column, MetaData, UnicodeText
 from migrate import migrate_engine
 from migrate.changeset import create_column, drop_column
 
 metadata = MetaData(migrate_engine)
 
 GroupsTable = Table('groups', metadata, autoload=True)
-col = Column("apply_rules", Text)
+col = Column('apply_rules', UnicodeText)
 
 def upgrade():
 	create_column(col, GroupsTable)
