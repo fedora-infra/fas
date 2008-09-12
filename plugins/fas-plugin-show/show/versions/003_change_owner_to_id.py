c0efc0754d8f90f8c77c4eb6634fbf7c9ff6060e
Yaakov M. Nemoy <loupgaroublond@gmail.com>
2008-09-12 15:24:38 -0400

    converts the owner from a string = users.username to an int = users.id

---
+++ plugins/fas-plugin-show/show/versions/003_change_owner_to_id.py
@@ -0,0 +1,49 @@
+from sqlalchemy import *
+from migrate import *
+from migrate.changeset.schema import *
+import pdb
+
+metadata = MetaData(migrate_engine)
+
+owner = Column('owner', Text)
+owner_id = Column('owner_id', Integer)
+
+shows_table = Table('show_shows', metadata,
+                    Column('id', Integer,
+                           autoincrement=True,
+                           primary_key=True),
+                    Column('name', Text),
+                    owner,
+                    Column('group_id', Integer,
+                           ForeignKey('groups.id')),
+                    Column('long_name', Text))
+
+PeopleTable = Table('people', metadata, autoload=True)
+
+def upgrade():
+    # Upgrade operations go here. Don't create your own engine; use the engine
+    # named 'migrate_engine' imported from migrate.
+    create_column(owner_id, shows_table)
+
+    owners = select([shows_table.c.id, shows_table.c.owner, 
+                     PeopleTable.c.id],
+                     shows_table.c.owner==PeopleTable.c.username).execute()
+    for x in owners:
+        shows_table.update(shows_table.c.id==x[0], values=dict(owner_id=x[2])).execute()
+    print owners
+    
+    drop_column(owner, shows_table)
+    pass
+
+def downgrade():
+    # Operations to reverse the above upgrade go here.
+    create_column(owner, shows_table)
+    
+    owners = select([shows_table.c.id, shows_table.c.owner_id, 
+                     PeopleTable.c.username],
+                     shows_table.c.owner_id==PeopleTable.c.id).execute()
+    for x in owners:
+        shows_table.update(shows_table.c.id==x[0], values=dict(owner=x[2])).execute()
+
+    drop_column(owner_id, shows_table)
+    pass
