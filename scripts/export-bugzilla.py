9271f7c5c648e26b6db02232270bbb4905f3b8e3
Toshio Kuratomi <toshio@fedoraproject.org>
2008-11-05 20:20:48 -0800

    Commit patch from jstanley to use python-bguzilla instead of calling xmlrpc methods manually.

--- scripts/export-bugzilla.py
+++ scripts/export-bugzilla.py
@@ -7,6 +7,7 @@ import sys
 import getopt
 import xmlrpclib
 import turbogears
+import bugzilla
 from turbogears import config
 turbogears.update_config(configfile="/etc/export-bugzilla.cfg")
 from turbogears.database import session
@@ -26,7 +27,7 @@ if __name__ == '__main__':
     ourGroup = args[0]
     bzGroup = args[1]
 
-    server = xmlrpclib.Server(BZSERVER)
+    server = bugzilla.Bugzilla(url=BZSERVER, user=BZUSER, password=BZPASS)
     bugzilla_queue = BugzillaQueue.query.join('group').filter_by(
             name=ourGroup)
 
@@ -35,8 +36,7 @@ if __name__ == '__main__':
         if entry.action == 'r':
             # Remove the user's bugzilla group
             try:
-                server.bugzilla.updatePerms(entry.email, 'rem', (bzGroup,),
-                        BZUSER, BZPASS)
+                server.updateperms(entry.email, 'rem', (bzGroup,))
             except xmlrpclib.Fault, e:
                 if e.faultCode == 504:
                     # It's okay, not having this user is equivalent to setting
@@ -48,7 +48,7 @@ if __name__ == '__main__':
         elif entry.action == 'a':
             # Try to create the user
             try:
-                server.bugzilla.addUser(entry.email, entry.person.human_name, BZUSER, BZPASS)
+                server.adduser(entry.email, entry.person.human_name)
             except xmlrpclib.Fault, e:
                 if e.faultCode == 500:
                     # It's okay, we just need to make sure the user has an
@@ -57,8 +57,7 @@ if __name__ == '__main__':
                 else:
                     print entry.email,entry.person.human_name
                     raise
-            server.bugzilla.updatePerms(entry.email, 'add', (bzGroup,),
-                    BZUSER, BZPASS)
+            server.updateperms(entry.email, 'add', (bzGroup,))
         else:
             print 'Unrecognized action code: %s %s %s %s %s' % (entry.action,
                     entry.email, entry.person.human_name, entry.person.username, entry.group.name)
