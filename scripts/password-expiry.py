52701cfba401f0327985be81771223a988d86d33
Ricky Zhou <ricky@fedoraproject.org>
2008-12-22 02:16:41 -0500

    Add password expiry script.

---
+++ scripts/password-expiry.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+__requires__ = 'TurboGears'
+import pkg_resources
+pkg_resources.require('CherryPy >= 2.0, < 3.0alpha')
+
+import pytz
+from datetime import timedelta
+
+import turbogears
+from turbogears import config
+turbogears.update_config(configfile="/etc/password-expiry.cfg")
+import turbomail
+from turbogears.database import session
+from fas.model import *
+from email.Message import Message
+import smtplib
+
+# TODO: GPG sign these emails.  
+# Maybe we can do it one-time and just include the signature in the message.
+
+WARN_AGE = timedelta(config.get('warn_age'))
+MAX_AGE = timedelta(config.get('max_age'))
+EXPIRY_TIME = config.get('max_age') - config.get('warn_age')
+SMTP_SERVER = config.get('smtp_server')
+
+# Taken (and slightly modified) from Toshio's pkgdb-sync-bugzilla.in in pkgdb
+def send_email(fromAddress, toAddress, subject, message, smtp_server=SMTP_SERVER):
+    '''Send an email if there's an error.
+    
+    This will be replaced by sending messages to a log later.
+    '''
+    msg = Message()
+    msg.add_header('To', toAddress)
+    msg.add_header('From', fromAddress)
+    msg.add_header('Subject', subject)
+    msg.set_payload(message)
+    smtp = smtplib.SMTP(smtp_server)
+    smtp.sendmail(fromAddress, [toAddress], msg.as_string())
+    smtp.quit()
+
+
+if __name__ == '__main__':
+    now = datetime.now(pytz.utc)
+    
+    people = People.query.all()
+    
+    for person in people:
+        if person.status != 'active':
+            # They're already deactivated.
+            continue
+        diff = now - person.password_changed
+        if diff > MAX_AGE:
+            person.status = 'inactive'
+            send_email(config.get('accounts_email'), person.email, 'Fedora Password Expiry', \
+            '''Your Fedora Account password has expired, so your account has been
+disabled.  To reenable your account, please request a password reset at
+https://admin.fedoraproject.org/accounts/user/resetpass.  
+
+If you have any questions, please feel free to contact us at
+accounts@fedoraproject.org.
+
+- Fedora Infrastructure Team
+''')
+        elif diff > WARN_AGE:
+            send_email(config.get('accounts_email'), person.email, 'Fedora Password Expiry Warning', \
+            '''Your Fedora Account password will expire in %(days)d days.  Please change your
+password at https://admin.fedoraproject.org/accounts/user/changepass
+as soon as possible to prevent your account from being disabled.  
+
+- Fedora Infrastructure Team
+''')
+    
+    session.flush()
+
