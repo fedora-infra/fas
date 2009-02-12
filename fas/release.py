7a553d6151a4f3403604b3dd1529a238f9a98283
Ricky Zhou <ricky@fedoraproject.org>
2009-02-12 13:13:15 -0500

    Prepare for 0.8.4.8 release.

--- fas/release.py
+++ fas/release.py
@@ -24,7 +24,7 @@
 Release information about the Fedora Accounts System
 '''
 
-VERSION = '0.8.4.7'
+VERSION = '0.8.4.8'
 NAME = 'fas'
 DESCRIPTION = 'The Fedora Account System'
 LONG_DESCRIPTION = '''
