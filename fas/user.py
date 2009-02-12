8d8cfa133cef15acbd32f6209aa0851aa2b4905b
Ricky Zhou <ricky@fedoraproject.org>
2009-02-12 13:09:39 -0500

    filter_private in user/dump.

--- fas/user.py
+++ fas/user.py
@@ -363,6 +363,7 @@ https://admin.fedoraproject.org/accounts/user/verifyemail/%s
         # p becomes what we send back via json
         p = []
         for strip_p in people:
+            strip_p.filter_private()
             if strip_p.status == 'active' and 'cla_done' in strip_p.group_roles:
                 p.append({
                     'username'  : strip_p.username,
