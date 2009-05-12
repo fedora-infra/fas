48c84b6b386d5d114b6dbc8e3f0b6640d61db104
Mike McGrath <mmcgrath@redhat.com>
2009-05-12 13:07:56 -0500

    Added yubikey auth

--- fas2.sql
+++ fas2.sql
@@ -85,7 +85,7 @@ CREATE TABLE configs (
     -- Please create more config keys rather than abusing this with
     -- large datastructures.
     value TEXT,
-    check (application in ('asterisk', 'moin', 'myfedora' ,'openid')),
+    check (application in ('asterisk', 'moin', 'myfedora' ,'openid', 'yubikey')),
     -- Might end up removing openid, depending on how far we take the provider
     unique (person_id, application, attribute)
 );
