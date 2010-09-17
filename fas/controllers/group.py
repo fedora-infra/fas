ccdb4e36d2f1a9213b9e8a161c30bd3229fc448f
Mike McGrath <mmcgrath@redhat.com>
2010-09-16 19:12:40 -0500

    fixed api connect / disconnect issues.
    Also added validators

--- fas/controllers/group.py
+++ fas/controllers/group.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """Main Controller"""
 
-from tg import expose, flash, require, url, request, redirect
+from tg import expose, flash, require, url, request, redirect, validate
+import tw.forms.validators as validators
 from pylons.i18n import ugettext as _, lazy_ugettext as l_
 from tgext.admin.tgadminconfig import TGAdminConfig
 from tgext.admin.controller import AdminController
@@ -17,16 +18,21 @@ from ipalib import api
 
 api.bootstrap(debug=False)
 api.finalize()
-api.Backend.xmlclient.connect()
 
+class GroupList(validators.Schema):
+    search = validators.UnicodeString(max=32)
 
 class Group(BaseController):
     """
         Group Controller for group listing and operations
     """
+
+    @validate(validators=GroupList())
     @expose('fas.templates.group.list')
     def list(self, search=u'a'):
         """Return a simple group list"""
+        api.Backend.xmlclient.connect()
         groups = api.Command.group_find(search)
+        api.Backend.xmlclient.disconnect()
         return dict(groups=groups, search=search)
 
