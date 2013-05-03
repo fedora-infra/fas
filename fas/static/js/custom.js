b6e56db0201dfdf82ba7286a47503824ae1f35ff
Ricky Elrod <ricky@elrod.me>
2013-05-03 03:30:03 -0400

    Start of group view port.

--- fas/static/js/custom.js
+++ fas/static/js/custom.js
@@ -15,4 +15,14 @@ $(function() {
             return $(this).parent().find('.content').html();
         }
     });
+
+    var hash = document.location.hash;
+    var prefix = "tab_";
+    if (hash) {
+        $('.nav-tabs a[href='+hash.replace(prefix,"")+']').tab('show');
+    }
+
+    $('.nav-tabs a').on('shown', function (e) {
+        window.location.hash = e.target.hash.replace("#", "#" + prefix);
+    });
 });
