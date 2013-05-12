728afe8c856e59c396da780fc5c729de2cf0fa2b
Ricky Elrod <ricky@elrod.me>
2013-05-12 09:49:35 -0400

    Nuke some samadhi remnants.

--- fas/model/__init__.py
+++ fas/model/__init__.py
@@ -1,5 +1,4 @@
 # We want to import everything from both models into the same namespace (W0401)
 # pylint: disable-msg=W0401
 from fasmodel import *
-from samadhi import *
 # pylint: enable-msg=W0401
