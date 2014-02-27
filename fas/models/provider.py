923e52890d6ad2b57735f141359d8a384b59285b
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-02-27 16:37:58 +0100

    Fix the filter/queries in the provider

--- fas/models/provider.py
+++ fas/models/provider.py
@@ -8,13 +8,13 @@ from fas.models.people import People
 
 def get_group_by_id(session, id):
     """ Retrieve a specific Groups by its id. """
-    query = session.query(Groups).filter(id == id)
+    query = session.query(Groups).filter(Groups.id == id)
     return query.first()
 
 
 def get_group_by_name(session, name):
     """ Retrieve a specific Groups by its name. """
-    query = session.query(Groups).filter(name == name)
+    query = session.query(Groups).filter(Groups.name == name)
     return query.first()
 
 
@@ -22,7 +22,7 @@ def get_group_by_name(session, name):
 
 def get_grouptype_by_id(session, id):
     """ Retrive a specific GroupType by its id. """
-    query = session.query(GroupType).filter(id == id)
+    query = session.query(GroupType).filter(GroupType.id == id)
     return query.first()
 
 
@@ -30,23 +30,23 @@ def get_grouptype_by_id(session, id):
 
 def get_people_by_id(session, id):
     """ Retrieve a specific People by its id. """
-    query = session.query(People).filter(id == id)
+    query = session.query(People).filter(People.id == id)
     return query.first()
 
 
 def get_people_by_username(session, username):
     """ Retrieve a specific People by its username. """
-    query = session.query(People).filter(username == username)
+    query = session.query(People).filter(People.username == username)
     return query.first()
 
 
 def get_people_by_email(session, email):
     """ Retrieve a specific People by its email. """
-    query = session.query(People).filter(email == email)
+    query = session.query(People).filter(People.email == email)
     return query.first()
 
 
 def get_people_by_ircnick(session, ircnick):
     """ Retrieve a specific People by its ircnick. """
-    query = session.query(People).filter(ircnick == ircnick)
+    query = session.query(People).filter(People.ircnick == ircnick)
     return query.first()
