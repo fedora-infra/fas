ff4766b36652a4a52395b94f4f6e4f996464900d
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-02-27 19:29:12 +0100

    Add generic __get_user and __get_group method and raise HTTPBadRequest where the request is wrong

--- fas/api/__init__.py
+++ fas/api/__init__.py
@@ -4,6 +4,7 @@ from pyramid.response import Response
 from pyramid.view import view_config
 
 from pyramid.httpexceptions import (
+    HTTPBadRequest,
     HTTPFound,
     HTTPNotFound,
 )
@@ -30,12 +31,9 @@ def api_home(request):
     return {}
 
 
-@view_config(route_name='api_user_get', renderer='json')
-def api_user_get(request):
-    key = request.matchdict.get('key')
-    value = request.matchdict.get('value')
+def __get_user(key, value):
     if key not in ['id', 'username', 'email', 'ircnick']:
-        raise HTTPNotFound(
+        raise HTTPBadRequest(
             {"error": "Bad request, no '%s' allowed" % key}
         )
     method = getattr(provider, 'get_people_by_%s' % key)
@@ -45,15 +43,12 @@ def api_user_get(request):
             {"error": "No such user %r" % value}
         )
 
-    return user.to_json()
+    return user
 
 
-@view_config(route_name='api_group_get', renderer='json')
-def api_group_get(request):
-    key = request.matchdict.get('key')
-    value = request.matchdict.get('value')
+def __get_group(key, value):
     if key not in ['id', 'name']:
-        raise HTTPNotFound(
+        raise HTTPBadRequest(
             {"error": "Bad request, no '%s' allowed" % key}
         )
     method = getattr(provider, 'get_group_by_%s' % key)
@@ -62,6 +57,23 @@ def api_group_get(request):
         raise HTTPNotFound(
             {"error": "No such group %r" % value}
         )
+
+    return group
+
+
+@view_config(route_name='api_user_get', renderer='json', request_method='GET')
+def api_user_get(request):
+    key = request.matchdict.get('key')
+    value = request.matchdict.get('value')
+    user = __get_user(key, value)
+    return user.to_json()
+
+
+@view_config(route_name='api_group_get', renderer='json', request_method='GET')
+def api_group_get(request):
+    key = request.matchdict.get('key')
+    value = request.matchdict.get('value')
+    group = __get_group(key, value)
 
     return group.to_json()
 
