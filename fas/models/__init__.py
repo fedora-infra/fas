a07064d53ae3048d09b5eeab07f1e2e45d6191bb
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-02-28 12:49:26 +0100

    Transform the AccountStatus and RoleLevel into tables

--- fas/models/__init__.py
+++ fas/models/__init__.py
@@ -1,6 +1,6 @@
-from flufl.enum import IntEnum
 from sqlalchemy.ext.declarative import declarative_base
 
+import sqlalchemy as sa
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
@@ -11,15 +11,14 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-class AccountStatus(IntEnum):
-    ACTIVE = 1
-    INACTIVE = 3
-    BLOCKED = 5
-    DISABLED = 8
 
-class RoleLevel(IntEnum):
-    UNKNOWN = 0
-    USER = 1
-    EDITOR = 2
-    SPONSOR = 3
-    ADMIN = 5
+class AccountStatus(Base):
+    __tablename__ = 'account_status'
+    id = sa.Column(sa.Integer, primary_key=True)
+    status = sa.Column(sa.Unicode(50), unique=True, nullable=False)
+
+
+class RoleLevel(Base):
+    __tablename__ = 'role_level'
+    id = sa.Column(sa.Integer, primary_key=True)
+    role = sa.Column(sa.Unicode(50), unique=True, nullable=False)
