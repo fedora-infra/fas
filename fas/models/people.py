10a5624b08f90ae1d6a1b3d9d1eba2062540c094
Xavier Lamien <laxathom@fedoraproject.org>
2014-02-28 17:08:46 +0100

    Add social network token's item to people model. Update some constraint from people model Update people activities model.

--- fas/models/people.py
+++ fas/models/people.py
@@ -16,13 +16,14 @@ from sqlalchemy import (
 
 import datetime
 
+
 class People(Base):
     __tablename__ = 'people'
     id = Column(Integer, Sequence('people_seq', start='10000'), primary_key=True)
     username = Column(Unicode(255), unique=True, nullable=False)
-    password = Column(Unicode(255), nullable=False)
+    password = Column(UnicodeText(), nullable=False)
     fullname = Column(UnicodeText(), nullable=False)
-    ircnick = Column(UnicodeText(), nullable=True)
+    ircnick = Column(UnicodeText(), unique=True, nullable=True)
     avatar = Column(UnicodeText(), nullable=True)
     postal_address = Column(UnicodeText(), nullable=False)
     country_code = Column(Unicode(2), nullable=True)
@@ -33,6 +34,7 @@ class People(Base):
     comment = Column(UnicodeText(), nullable=True)
     timezone = Column(UnicodeText(), default=u'UTC')
     gpg_id = Column(UnicodeText(), nullable=True)
+    gpg_fingerprint = Column(UnicodeText(), nullable=True)
     ssh_key = Column(UnicodeText(), nullable=True)
     email = Column(UnicodeText(), unique=True, nullable=False)
     email_token = Column(UnicodeText(), unique=True, nullable=True)
@@ -45,27 +47,32 @@ class People(Base):
     status = Column(Enum, default=1)
     status_change = Column(DateTime, default=datetime.datetime.utcnow)
     privacy = Column(Boolean, default=False)
-    alias_enabled = Column(Boolean, default=True)
+    email_alias = Column(Boolean, default=True)
     blog_rss = Column(UnicodeText(), nullable=False)
     latitude = Column(Numeric, nullable=True)
     longitude = Column(Numeric, nullable=True)
+    fas_token = Column(UnicodeText(), nullable=True)
+    github_token = Column(UnicodeText(), nullable=True)
+    twitter_token = Column(UnicodeText(), nullable=True)
     last_logged = Column(DateTime, default=datetime.datetime.utcnow)
 
 Index('people_username_idx', People.username)
 
-class PeopleAccessLog(Base):
-    __tablename__ = 'people_access_log'
+
+class PeopleAccountActivitiesLog(Base):
+    __tablename__ = 'people_activity_log'
     id = Column(Integer, primary_key=True)
     people = Column(Integer, ForeignKey('people.id'), nullable=False)
-    access_from = Column(Numeric, nullable=False)
-    access_through = Column(UnicodeText(), nullable=False)
-    access_timestamp = Column(DateTime, default=datetime.datetime.utcnow())
+    location = Column(Numeric, nullable=False)
+    access_from = Column(UnicodeText(), nullable=False)
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow())
 
-Index('people_access_log_idx', PeopleAccessLog.access_from)
+Index('account_access_log_idx', PeopleAccountActivitiesLog.location)
+
 
 class PeopleVirtualAccount(Base):
-    __tablename__ = 'virtual_user'
-    id = Column(Integer, unique, primary_key=True)
+    __tablename__ = 'virtual_people'
+    id = Column(Integer, unique=True, primary_key=True)
     username = Column(UnicodeText(), unique=True, nullable=False)
     parent = Column(Integer, ForeignKey('people.id'), nullable=False)
     type = Column(Integer, default=1)
