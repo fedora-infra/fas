970a9a953a5083098db9f4b20043820f2f68ad36
Xavier Lamien <laxathom@fedoraproject.org>
2014-08-29 23:12:18 +0200

    Allow recovery email to be NULL.

--- fas/models/people.py
+++ fas/models/people.py
@@ -48,7 +48,7 @@ class People(Base):
     gpg_fingerprint = Column(UnicodeText(), nullable=True)
     ssh_key = Column(UnicodeText(), nullable=True)
     email = Column(UnicodeText(), unique=True, nullable=False)
-    recovery_email = Column(UnicodeText(), unique=True, nullable=False)
+    recovery_email = Column(UnicodeText(), unique=True, nullable=True)
     bugzilla_email = Column(UnicodeText(), unique=True, nullable=True)
     email_token = Column(UnicodeText(), unique=True, nullable=True)
     unverified_email = Column(UnicodeText(), nullable=True)
