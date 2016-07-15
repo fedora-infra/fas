270c2357e5012caad2093f6aa0aa2298fc2f2e6f
skrzepto <shims506@gmail.com>
2016-07-15 14:11:23 -0500

    basic test_views_people

---
+++ tests/test_views_people.py
@@ -0,0 +1,18 @@
+import unittest
+from tests import BaseTest
+
+
+class ViewsPeopleFunctionalTests(BaseTest):
+
+    def test_people_get(self):
+        res = self.testapp.get('/people', status=200)
+        self.assertTrue('<div class="container people-list">' in res.body)
+        # Jerry is the only registered and confirmed account from default data
+        self.assertTrue('<a href="http://localhost/people/profile/jerry">'
+                        in res.body)
+
+
+if __name__ == '__main__':
+    SUITE = unittest.TestLoader().loadTestsFromTestCase(
+        ViewsPeopleFunctionalTests)
+    unittest.TextTestRunner(verbosity=2).run(SUITE)
