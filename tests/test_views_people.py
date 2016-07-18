42a7d0e353d95d36e7cd5cc2fdf51a72a86a5aaf
skrzepto <shims506@gmail.com>
2016-07-18 12:21:37 -0500

    adding search tests

--- tests/test_views_people.py
+++ tests/test_views_people.py
@@ -15,6 +15,16 @@ class ViewsPeopleFunctionalTests(BaseTest):
         res = self.testapp.get('/people/profile/jerry', status=200)
         self.assertTrue('<dd>jerry</dd>' in res.body)
 
+    def test_people_partial_search(self):
+        res = self.testapp.get('/people/search/j', status=200)
+        self.assertTrue('<h3>Search for people</h3>' in res.body)
+        self.assertTrue('<a href="http://localhost/people/profile/jerry">' in res.body)
+
+    def test_people_search_name(self):
+        res = self.testapp.get('/people/search/jerry', status=302)
+        res = res.follow()
+        self.assertTrue('<dd>jerry</dd>' in res.body)
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(
