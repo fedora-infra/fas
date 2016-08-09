b5ab34010a1f45849ec078b2ea9e51cdd0453820
Patrick Uiterwijk <puiterwijk@redhat.com>
2016-08-09 23:13:23 +0000

    Bump version to 0.14.0
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- fas/release.py
+++ fas/release.py
@@ -25,7 +25,7 @@
 Release information about the Fedora Accounts System
 '''
 
-VERSION = '0.13.1'
+VERSION = '0.14.0'
 NAME = 'fas'
 DESCRIPTION = 'The Fedora Account System'
 LONG_DESCRIPTION = '''
